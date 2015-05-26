2f49bea3e6b2f035ed0de07589a137c1248e7938
YAMAHA Harada <yamaha.harada@gmail.com>
2015-05-26 20:05:04 +0900

    initial commit.

--- BlueLibrary/ViewController.swift
+++ BlueLibrary/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // リクエスト
+        let manager:AFHTTPRequestOperationManager = AFHTTPRequestOperationManager()
+        let serializer:AFJSONRequestSerializer = AFJSONRequestSerializer()
+        manager.requestSerializer = serializer
+        manager.GET("http://ip.jsontest.com/", parameters: nil,
+            success: {(operation: AFHTTPRequestOperation!, responsobject: AnyObject!) in
+                println("Success!!")
+                println(responsobject)
+            },
+            failure: {(operation: AFHTTPRequestOperation!, error: NSError!) in
+                println("Error!!")
+            }
+        )
     }
 
     override func didReceiveMemoryWarning() {
